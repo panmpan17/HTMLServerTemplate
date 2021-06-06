709e88e81ac709baa65ed460f668aa6d77d7f974
Michael Pan <panmpan@gmail.com>
2021-06-06 10:55:21 +0800

    Set up static file loader

--- src/view.py
+++ src/view.py
@@ -1,10 +1,24 @@
 import cherrypy
+import os
+import jinja2
 
+from datetime import datetime
 from .rest_api import MainRestViewHandler
 
 
+TIMESTAMP = str(int(datetime.now().timestamp()))
+
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(
+        os.path.dirname(__file__), "templates")))
+
+
 class ViewHandler:
-    pass
+    @staticmethod
+    def render_html(html_file, **params):
+        t = jinja_env.get_template(html_file)
+        return t.render(params)
 
 class MainViewHandler(ViewHandler):
     def __init__(self):
@@ -12,4 +26,12 @@ class MainViewHandler(ViewHandler):
 
     @cherrypy.expose
     def index(self, **kwargs):
-        return "Index"
+        return self.render_html("index.html")
+
+    @cherrypy.expose
+    def timestamp(self, **kwargs):
+        """
+        Return the time stamp when the server starts.
+        Use in loading newest static files
+        """
+        return TIMESTAMP
